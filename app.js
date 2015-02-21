6d076ec4e4dc1a0981c58d85585509143531b3b3
Ross Anderson <ross.anderson@ualberta.ca>
2015-02-21 10:13:51 -0500

    Add yeg-public-art.json, move views into /public for possible reuse

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = module.exports = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/public/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
